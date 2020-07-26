c791061e190a61c1404a1322c00a458c9552cca3
Andres Rivera <soulkalir@gmail.com>
2020-07-26 19:41:36 +0000

    Automatically backed up by Learn

--- js/index.js
+++ js/index.js
@@ -4,3 +4,13 @@ var height = 70
 // Don't worry about this
 module.exports = { name, height
 }
+<<<<<<< HEAD
+=======
+
+if (name === "Susan") {
+  console.log("The name is correct")
+}
+else {
+  console.log("Expected the name to be Susan")
+}
+>>>>>>> 1fadef8e15869b43630c0b5dc8540b131427467f
--- js/other_file.js
+++ js/other_file.js
@@ -1,3 +1,7 @@
+<<<<<<< HEAD
 console.log()
 var index = require ("./index.js")
 console.log(index.name)
+=======
+console.log(Joe)
+>>>>>>> 1fadef8e15869b43630c0b5dc8540b131427467f
